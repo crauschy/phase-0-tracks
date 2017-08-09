a9ecb46c6658c50d3108b752486d776fa52aca93
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-09 11:56:58 -0400

    Add data conversions

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -6,7 +6,7 @@ name = gets.chomp
 #Ask the volume level of hamster 1-10.
 
 puts "On as scale of 1-10, 10 being loudest, what is the volume level of #{name}?"
-volume = gets.chomp
+volume = gets.to_i
 
 #Ask their fur-color
 
@@ -17,14 +17,19 @@ fur = gets.chomp
 
 puts "Is #{name} a good candidate for adoption-- y/n?"
 adopt = gets.chomp
+if adopt == "y"
+  adopt = true
+else
+  adopt = false
+end
 
 #Ask for estimated age.
 
 puts "About how old do you think #{name} is?"
-age = gets.chomp
+age = gets.to_f
 
   #Set unanswered est. age to `nil`
-
 if age == ""
   age = nil
+  puts age
   end
