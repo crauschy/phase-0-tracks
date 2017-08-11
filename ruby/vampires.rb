a544461789a0b3687d65e940ce350a189f21a79a
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-11 16:13:27 -0400

    Initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,4 @@
+#What is your name? Some known vampires are in the area, and we can check against the Werewolf Intelligence Bureau database for their aliases.
+#How old are you? What year were you born? This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
+#Our company cafeteria serves garlic bread. Should we order some for you? Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
+#Would you like to enroll in the company’s health insurance? Vampires are immortal, so they certainly don’t need health insurance.
