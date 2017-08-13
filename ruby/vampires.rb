4a404905ca0b68c2523dc7dd42620db6433d979f
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-13 13:12:49 -0400

    Fix variable reassignments

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,18 +1,38 @@
-#What is your name? Some known vampires are in the area, and we can check against the Werewolf Intelligence Bureau database for their aliases.
+#What is your name?
 puts "What is your name?"
 emp_name = gets.chomp
 
-#How old are you? What year were you born? This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
+#How old are you? What year were you born?
 puts "What is your age?"
 emp_age = gets.to_i
 
 puts "In what year were you born?"
 emp_birthyear = gets.to_i
-
-#Our company cafeteria serves garlic bread. Should we order some for you? Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
+current_year = Time.new.year
+#Should we order some for you?
 puts "Would you like some garlic bread from the cafeteria? y/n"
 emp_likesgarlic = gets.chomp
-
-#Would you like to enroll in the company’s health insurance? Vampires are immortal, so they certainly don’t need health insurance.
+if emp_likesgarlic == "y" || emp_likesgarlic == "yes"
+  emp_likesgarlic = true
+else emp_likesgarlic = false
+end
+#Would you like to enroll in the company’s health insurance?
 puts "Would you like to enroll in the company's health insurance? y/n"
-emp_healthinsure = gets.chomp
+emp_needshealth = gets.chomp
+if emp_needshealth == "y" || emp_needshealth == "yes"
+ emp_needshealth = true
+else emp_needshealth = false
+end
+
+puts emp_name
+puts emp_age
+puts emp_birthyear
+puts emp_likesgarlic
+puts emp_needshealth
+
+#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+if emp_age == (current_year - emp_birthyear) && (emp_likesgarlic || emp_needshealth)
+  vampire_status = "Probably not a vampire"
+else vampire_status = "n/a"
+end
+p vampire_status
