4bbb4d91382908cc7f42ed9e8e6aa1ecad1a3db8
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-14 09:35:28 -0400

    Reorganize Method Declarations and Driver Code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,5 @@
+#METHOD DECLARATIONS
+
 #1: ENCRYPT METHOD
 #Ask user for "password" to encrypt. Assign password variable.
 
@@ -5,25 +7,27 @@
 #password = gets.chomp
 
 #Define encrypt method that advances each letter of password string one letter forward in alphabet:
-  #Assign counter variable.
-  #Write loop that will continue rewriting each letter of password as the next letter in alphabet (redefining the password variable with its encryption)
 def encrypt(x)
+  #Assign counter variable.
   counter = 0
-#Print password to be encrypted.
+  #Print password to be encrypted.
 puts "Ok, so the password you wish to encrypt is:
   [#{x}]
 Easy peasy... Your password will be encrypted..."
-
+  #Write loop to rewrite each letter as next letter in alphabet
   while counter < x.length
     if x[counter] == "z"
       x[counter] = "a"
     else x[counter] =
       x[counter].next!
     end
+    #advance the counter
     counter += 1
   end
+  #Print encrypted password
   puts "Your password is now encrypted as:
   [#{x}]"
+  #Return (now encrypted) password
   return x
 end
 
@@ -36,22 +40,32 @@ end
 #pause = gets.chomp
 
 #2: DECRYPT METHOD
-#Define a decrypt method that will reverse our encrypt method and return the original password.
-  #Define new counter variable.
-  #Define an empty string.
-  #Write a loop that goes through each letter of our encrypted password, defines its position as an index of the alphabet, moves back a position in the alphabet, and then pushes the letter corresponding to the new position into our empty string, returning a new string that is our original password.
+#Reverse encrypt method to return to the original password
+
+
+  #then pushes the letter corresponding to the new position into our empty string, returning a new string that is our original password.
 
 def decrypt(x)
+  #Define new counter variable.
   counter = 0
+  #Declare an empty string
   decrypted_password = ""
+  #Loop through each letter of encrypted password to return to original letter
   while counter < x.length
+    #assign alphabet string
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    #define letter's position as an index of the alphabet,
     encrypted_position = alphabet.index(x[counter])
+    #assign new variable to move back a position in the alphabet
     decrypted_position = encrypted_position - 1
+    #push new letters into empty string
     decrypted_password<<alphabet[decrypted_position]
+    #advance the counter
     counter += 1
   end
+  #print decrypted password
   puts "Your original password was #{decrypted_password}."
+  #return decrypted password
   decrypted_password
 end
 
@@ -65,13 +79,17 @@ end
 #p decrypt(encrypt("swordfish"))
 
 
-#Ask user whether decrypt or encrypt a password
+#DRIVER CODE
+
+#Ask user whether to decrypt or encrypt a password
 puts "Hello Secret Agent. Would you like to decrypt or encrypt a password?"
 crypt_direction = gets.chomp
+
 #Ask for the password
 puts "What is the password you would like to #{crypt_direction}?"
 password = gets.chomp
-#Does operation; prints results to screen
+
+#Does chosen operation
 if crypt_direction == "encrypt"
   encrypt(password)
 elsif crypt_direction == "decrypt"
