c6f1bd005ba8fc6907cf49298961e4017a0d0567
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-19 15:57:16 -0400

    Initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+#CLIENT DETAILS PROGRAM
+
+#METHOD DECLARATIONS
+
+#Ask for and convert to appropriate data type
+name - string
+age - integer
+kids - integer
+decor_theme - s
+rooms_wanted
+budget
+
+#Print entire hash
+
+#Give opportunity to update:
+  #"none" will skip
+  #"[key]" will prompt for new value and update hash
+
+#Print updated hash
+
+#DRIVER CODE
