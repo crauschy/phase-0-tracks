34b613309280d89459c3c7b502903f3431211995
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-19 22:31:46 -0400

    Write pseudocode for insertion method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,39 +1,66 @@
-#Paired with Noah Krell
-#Release 0: Implement a Simple Search
+# # Paired with Noah Krell
+# # Release 0: Implement a Simple Search
 
-#write and print sample array
+# # write and print sample array
 
-favorite_nums = [2,3,7,22,42,77]
-p favorite_nums
+# favorite_nums = [2,3,7,22,42,77]
+# p favorite_nums
 
-#define method that returns indext of searched-for integer
+# # define method that returns indext of searched-for integer
 
-def search_array(favorite_nums,num)
-  #loop containing a conditional statement
-  iteration = 0
-  while iteration < favorite_nums.length
-    if num == favorite_nums[iteration]
-      return iteration
-    end
-    iteration += 1
-  end
-end
+# def search_array(favorite_nums,num)
+#   # loop containing a conditional statement
+#   iteration = 0
+#   while iteration < favorite_nums.length
+#     if num == favorite_nums[iteration]
+#       return iteration
+#     end
+#     iteration += 1
+#   end
+# end
 
-#run and test method
-p search_array(favorite_nums,22)
+# # run and test method
+# p search_array(favorite_nums,22)
 
-p search_array(favorite_nums,45)
+# p search_array(favorite_nums,45)
 
-#Release 1:
 
-def fib(num)
- i = 0
- numbers = [0, 1]
- until i == num - 2
-  x = numbers[-1] + numbers[-2]
-  numbers.push(x)
- i += 1
- end
- numbers
-end
-p fib(6)
+# # Release 1: Calculate Fibonacci Numbers
+
+# def fib(num)
+#  i = 0
+#  numbers = [0, 1]
+#  until i == num - 2
+#   x = numbers[-1] + numbers[-2]
+#   numbers.push(x)
+#  i += 1
+#  end
+#  numbers
+# end
+
+# p fib(6)
+# # Verification of Large Numbers
+# p fib(100).last
+
+
+# Release 2: Insertion Sorting Method
+# an insertion sorting method goes through an array and if an element is smaller than the one to its left, it shifts
+
+#loop through array starting with the second element (E), compare E to the element to its left
+#if the left > E, shift the left element to the right
+  #continue to shift until either there is no element to the left or the element to the left is <= E
+  #insert E to this new position
+#repeat with the next element, E
+#until index = length of the array (and all elements are sorted)
+
+# Implement the sorting method in Ruby.
+
+array = [5,3,1,8,7,12]
+#expected outcome: [1,3,5,7,8,12]
+
+#loop through array starting with the second element (E), compare E to the element to its left
+#if the left > E, shift the left element to the right
+  #continue to shift until either there is no element to the left or the element to the left is <= E
+  #insert E to this new position
+#repeat with the next element, E
+#until index = length of the array (and all elements are sorted)
