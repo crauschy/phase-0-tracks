d84a563e9d76a708a2f209e6a23d5e5de25df433
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-23 14:36:17 -0400

    Complete create_list method and driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,22 +1,22 @@
 #////// METHOD DECLARATIONS ///////#
 
-# Method to create a list
+# Method to CREATE A LIST
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
 def create_list(grocery_list)
   # grocery_list will be a string from user
-  # assign an empty hash (will be the list)
+  # assign an empty hash (will eventually be the list)
   final_list = {}
   # use split method to get list items
   grocery_list = grocery_list.split
-  # Iterate through the elements of the array
-  grocery_list.each { |item| final_list[item]=1}
+  # Iterate through the elements of the array and insert to hash
   # set default quantity
+  grocery_list.each { |item| final_list[item]=1}
   # print the list to the console [can you use one of your other methods here?]
   final_list
 end
 # output: [hash]
-our_list = create_list(grocery_list)
+
 
 
 # Method to add an item to a list
@@ -40,5 +40,16 @@ our_list = create_list(grocery_list)
 # steps:
 # output:
 
+#////// DRIVER CODE ///////#
+
+# Ask user for list input
+puts "Welcome to THE LIST MAKER!"
+
+puts "What do you need to get at the grocery store?
+Type everything you need and then hit 'enter'."
+
+grocery_list = gets.chomp
+
+p create_list(grocery_list)
 
 
