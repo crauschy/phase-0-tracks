7e351022cd61e241a18da9aaaeb996c0405919ab
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-23 14:58:08 -0400

    Define add_item method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -17,14 +17,24 @@ def create_list(grocery_list)
 end
 # output: [hash]
 
+grocery_list = "carrots apples bananas eggs milk"
+list_1 = create_list(grocery_list)
 
-
-# Method to add an item to a list
+# Method to ADD AN ITEM to a list
 # input: list, item name, and optional quantity
-# steps: our_list[item] = quantity
+def add_item(list, item, quantity=1)
+# steps:
+  # Add item as key and quantity as value to final_list hash(item and quantity will be user inputs)
+  list[item] = quantity
+  # Return updated hash
+  list
+end
+
+item = "pizza"
+quantity = 3
 
 # output:
-
+p add_item(list_1, item, quantity)
 # Method to remove an item from the list
 # input:
 # steps:
@@ -42,14 +52,15 @@ end
 
 #////// DRIVER CODE ///////#
 
-# Ask user for list input
-puts "Welcome to THE LIST MAKER!"
+# # Ask user for list input
+# puts "Welcome to THE LIST MAKER!"
 
-puts "What do you need to get at the grocery store?
-Type everything you need and then hit 'enter'."
+# puts "What do you need to get at the grocery store?
+# Type everything you need and then hit 'enter'."
 
-grocery_list = gets.chomp
+# grocery_list = gets.chomp
+# # Call create_list method
+# p create_list(grocery_list)
 
-p create_list(grocery_list)
-
+# # Ask user if they would like to ADD or REMOVE or UPDATE and item from list; loop until 'done'
 
