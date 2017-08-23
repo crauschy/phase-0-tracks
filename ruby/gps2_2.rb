93d24f398357b0a28f2a9e84db1b5f9627783a00
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-23 18:13:57 -0400

    Debug driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -8,10 +8,10 @@ def create_list(title, list_arr)
   # assign an empty hash (will eventually be the list)
   final_list = {}
   # use split method to get list items
-  grocery_list = grocery_list.split
+  list_arr = list_arr.split
   # Iterate through the elements of the array and insert to hash
   # set default quantity
-  grocery_list.each { |item| final_list[item]=1}
+  list_arr.each { |item| final_list[item]=1}
   # print the list to the console [can you use one of your other methods here?]
   puts print_list(title,final_list)
   final_list
@@ -24,7 +24,7 @@ end
 def add_item(list, add_item, add_quantity=1)
 # steps:
   # Add item as key and quantity as value to final_list hash(item and quantity will be user inputs)
-  list[item] = quantity
+  list[add_item] = add_quantity
   # Return updated hash
   list
 end
@@ -36,7 +36,7 @@ end
 def remove_item(list, rm_item)
 # steps:
   # use delete method with key (item) as argument
-  list.delete(item)
+  list.delete(rm_item)
   # return list
   list
 end
@@ -48,7 +48,7 @@ end
 def update_quantity(list, upd_item, new_quantity)
 # steps:
   # reassign key (item) a new value (quantity)
-  list[item] = new_quantity
+  list[upd_item] = new_quantity
   # return list
   list
 end
@@ -66,7 +66,7 @@ def print_list(title,list)
   puts "   #    ITEM"
   puts "----------------------"
   # print each item and it's quantity, bulleted if possible
-  list.each {|item,quantity| puts "  #{quantity}    #{item}"}
+  list.each {|item,quantity| puts "   #{quantity}    #{item}"}
   puts "**********************"
   # print today's date
   date = Time.new
@@ -90,68 +90,69 @@ title = gets.chomp
 our_list = create_list(title,initial_list)
 
 # Ask user if they'd like to ADD or REMOVE or UPDATE and item from list or type 'done';
-change = ""
-  while change != "done"
+loop do
+  change = ""
   puts "Need to make a change? Type:
     * 'add' to add something to your list
     * 'remove' to delete something from your list
     * 'update' to change the quantity of something, or
     * 'done' if everything looks alright"
-  change = get.chomp!
+  change = gets.chomp!
       # IF add, ask for quantity and item,update list hash; loop; must type 'done' when done adding
-      if change == "add"
-        puts "Type an item to add, or put 'done' to go back:"
-        loop do
-          add_item = gets.chomp
-          if add_item != "done"
-            puts "How many?"
-            add_quantity = gets.chomp.to_i
-            our_list = add_item(our_list, add_item, add_quantity=1)
-            puts "Item added.
-            Type another item, or 'done' to exit:"
-          else
-            puts print_list(our_list)
-            break
-          end
-        end
-      # IF remove, ask for item, and update list hash
-      elsif change == "remove"
-        puts "Type the item you wish to remove or 'done' to go back:"
-        loop do
-          rm_item = gets.chomp
-          if rm_item != "done"
-            our_list = remove_item(our_list,rm_item)
-            puts "Item removed.
-            Type another item to remove, or 'done' to exit:"
-          else
-            puts print_list(our_list)
-            break
-          end
-        end
-      # IF update, ask for new quantity and item, update hash; loop; must type 'done' when done updating
-      elsif change == "update"
-        puts "Type the item you'd like to update or 'done' to go back:"
-        loop do
-          upd_item = gets.chomp
-          if upd_item != "done"
-            puts "What is the correct quantity?"
-            new_quantity = gets.chomp.to_i
-            our_list = update_quantity(our_list)
-            puts "Item updated!
-            Type another item to update its quantity or 'done' to exit:"
-          else
-            puts print_list(our_list)
-            break
-          end
-        end
+  if change == "add"
+    puts "Type an item to add, or put 'done' to go back:"
+    loop do
+      add_item = gets.chomp
+      if add_item != "done"
+        puts "How many?"
+        add_quantity = gets.chomp.to_i
+        our_list = add_item(our_list, add_item, add_quantity=1)
+        puts "Item added."
+        puts "Type another item to add, or 'done' to exit:"
       else
-        puts "LIST MAKER did not understand your request."
+        puts print_list(title,our_list)
+        break
       end
     end
+  # IF remove, ask for item, and update list hash
+  elsif change == "remove"
+    puts "Type the item you wish to remove or 'done' to go back:"
+    loop do
+      rm_item = gets.chomp
+      if rm_item != "done"
+        our_list = remove_item(our_list,rm_item)
+        puts "Item removed."
+        puts "Type another item to remove, or 'done' to exit:"
+      else
+        puts print_list(title,our_list)
+        break
+      end
+    end
+  # IF update, ask for new quantity and item, update hash; loop; must type 'done' when done updating
+  elsif change == "update"
+    puts "Type the item you'd like to update or 'done' to go back:"
+    loop do
+      upd_item = gets.chomp
+      if upd_item != "done"
+        puts "What is the correct quantity?"
+        new_quantity = gets.chomp.to_i
+        our_list = update_quantity(our_list, upd_item, new_quantity)
+        puts "Item updated!"
+        puts "Type another item to update its quantity or 'done' to exit:"
+      else
+        puts print_list(title,our_list)
+        break
+      end
+    end
+  elsif change == "done"
+    break
+  else
+    puts "LIST MAKER did not understand your request."
+  end
+end
   # loop until 'done'
 # When 'done', thank user and print updated list
-puts print_list(our_list)
-
+puts puts print_list(title,our_list)
 puts "----------------------
 Thank you for using THE LIST MAKER!!!
 ----------------------"
