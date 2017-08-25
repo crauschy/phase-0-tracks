ef4f244825d9fa5de624cb4bb2504de37251e3f8
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-25 11:38:44 -0400

    Finish creating red_panda collection

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,58 +1,71 @@
-# #/////Method Declaration/////
-# class Puppy
-#   def fetch(toy)
-#     puts "I brought back the #{toy}!"
-#     toy
-#   end
-#   def speak(i)
-#       i.times {puts "Woof!"}
-#   end
-#   def roll_over
-#       puts "*rolls over*"
-#   end
-#   def dog_years(human_years)
-#       dog_age = human_years * 7
-#       p dog_age
-#   end
-#   def stay(seconds)
-#       stay = "." * seconds
-#       puts "stay#{stay}okay!"
-#   end
-#   def initialize
-#       puts "Initializing new puppy instance..."
-#   end
-# end
-# #///////Driver Code//////
-#  fluffy = Puppy.new
-#  fluffy.fetch("ball")
-#  fluffy.speak(4)
-#  fluffy.roll_over
-#  fluffy.dog_years(2)
-#  fluffy.stay(10)
+#/////Method Declaration/////
+class Puppy
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+  def speak(i)
+      i.times {puts "Woof!"}
+  end
+  def roll_over
+      puts "*rolls over*"
+  end
+  def dog_years(human_years)
+      dog_age = human_years * 7
+      p dog_age
+  end
+  def stay(seconds)
+      stay = "." * seconds
+      puts "stay#{stay}okay!"
+  end
+  def initialize
+      puts "Initializing new puppy instance..."
+  end
+end
+#///////Driver Code//////
+ fluffy = Puppy.new
+ fluffy.fetch("ball")
+ fluffy.speak(4)
+ fluffy.roll_over
+ fluffy.dog_years(2)
+ fluffy.stay(10)
+
 #/////Method Declaration////
 class Red_Panda
-    def initialize
-        puts "Initializing new red panda instance..."
+    def initialize(name)
+        @name = name
+        puts "Welcome, #{name}..."
     end
     def pounce(i)
-        i.times {puts "pounce..."}
+        i.times {puts "#{@name} pounced..."}
     end
-    def sleeping(red_panda)
-        puts "#{red_panda} is sleeping"
+    def sleepy(i)
+        sleeps = "sleepy"
+        sleeps.sub!("ee", 'ee'* i)
+        puts "#{@name} is #{sleeps}"
     end
 end
 #//////Driver Code//////
-counter = 0
-red_pandas = []
-until counter == 50
-    idgy0 = Red_Panda.new
-    counter += 1
-end
-p Red_Panda
-puts "
-These are the red pandas:
+# idgy = Red_Panda.new("Idgy")
 
-#{red_pandas}"
-red_pandas.each do |idgy|
-    idgy.sleeping(idgy)
+# idgy.sleepy(8)
+# idgy.pounce(3)
+
+#create array of 50 names
+counter = 1
+names = []
+until counter == 51
+    names << "Idgy#{counter}"
+    counter +=1
+end
+#create empty array for collection
+red_pandas = []
+# for each name, make instance of Red_Panda class
+names.each do |name|
+    red_pandas << Red_Panda.new(name)
+end
+
+red_pandas.each do |red_panda|
+    red_panda.pounce(3)
+    red_panda.sleepy(4)
 end
