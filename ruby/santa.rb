7c3f18d5df2c16ab43d22d124bb3523d1499e8f2
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-08-25 19:29:46 -0400

    Add getter and setter methods as attr_reader and attr_accessor and test

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,8 @@
 #//// CLASS AND METHOD DECLARTIONS ////#
 
 class Santa
+  attr_reader :age, :ethnicity, :reindeer_ranking
+  attr_accessor :gender
 
   def initialize(gender,ethnicity)
     puts "Initializing Santa instance..."
@@ -10,14 +12,37 @@ class Santa
     @age = 0
   end
 
+  # # setter methods
+  # def gender=(new_gender)
+  #   @gender = new_gender
+  # end
+
+  # # getter methods
+  # def age
+  #   @age
+  # end
+
+  # def ethnicity
+  #   @ethnicity
+  # end
+
+  # other methods
   def speak
-    puts "Ho, ho, ho! Haaaapy holidays!"
+    puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type} cookie!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking[8] = reindeer_name
+  end
 
 
 end
@@ -32,4 +57,25 @@ gender_options.length.times do |index|
   santas << Santa.new(gender_options[index], ethnicity_options[index])
 end
 
-p santas
+p santas[0]
+
+puts "Santa's ranking of reindeer BEFORE getting mad at Vixen:"
+p santas[0].reindeer_ranking
+santas[0].get_mad_at("Vixen")
+puts "Santa's ranking of reindeer AFTER getting mad at Vixen:"
+p santas[0].reindeer_ranking
+
+puts "This santa was #{santas[0].age}."
+
+santas[0].celebrate_birthday
+
+puts "This santa is now #{santas[0].age}."
+
+puts "This santa is not #{santas[0].gender}..."
+santas[0].gender=("bigender")
+puts "They are #{santas[0].gender}."
+
+puts "This santa is #{santas[0].ethnicity}."
+
+
+
