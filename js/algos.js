bd3365c908040758caa9ecd0093f521b66bfa0e4
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-09-04 12:50:32 -0400

    Write longest string funtion

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+var ary = ["cat", "horse", "elephant"];
+
+function longestWord(array){
+  var winner = array[0];
+  // loop through each element of array
+  for (var i = 1; i < array.length; i++){
+  // find each length, compare it to the element before
+    // if that element is longer, return it
+  // if element before is longer, return it
+    if (array[i].length > winner.length){
+      var winner = array[i]
+    };
+  };
+  return winner;
+}
+
+console.log(ary)
+console.log((longestWord(ary)))
+
+// The above works in repl.it.... I don't know why it's not working on node in command line :(
