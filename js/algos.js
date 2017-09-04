0cba975a23be8f2fc9cc578a0a097acf501a172c
Cate Rauschenberger <cate.rauschenberger@gmail.com>
2017-09-04 13:20:20 -0400

    Pseudocode key-value match search function

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,5 @@
 
+////////// RELEASE 0 ///////////
 
 function longestWord(array){
   var winner = array[0];
@@ -14,16 +15,23 @@ function longestWord(array){
   return winner;
 }
 
+// var ary1 = ["cat", "horse", "elephant"];
+// var ary2 = ["this is long", "this is longer", "this is longest", "or maybe this is longest"];
 
+// console.log("The longest element of")
+// console.log(ary1)
+// console.log("is:")
+// console.log((longestWord(ary1)))
 
-var ary1 = ["cat", "horse", "elephant"];
-var ary2 = ["this is long", "this is longer", "this is longest", "or maybe this is longest"];
+// console.log("The longest element of ary2 is:")
+// console.log((longestWord(ary2)))
 
-console.log("The longest element of")
-console.log(ary1)
-console.log("is:")
-console.log((longestWord(ary1)))
+////////// RELEASE 1 ///////////
 
-console.log("The longest element of ary2 is:")
-console.log((longestWord(ary2)))
+// compare two objects
+// do any keys match; if not, return false
+// if yes, loop through the matched keys and compare the respective values;
+// once a value ever matches, break loop and return true
+// if no values match, return false
 
+function doTheyShare(object1,object2)
